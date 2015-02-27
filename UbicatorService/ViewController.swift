9523fd620b8187287f41087dcffee148093836ea
Leonardo Nuñez <Leo@iMac-de-Leonardo.local>
2015-02-26 21:55:29 -0300

    Initial Commit

---
+++ UbicatorService/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UbicatorService
+//
+//  Created by Leonardo Nuñez on 26-02-15.
+//  Copyright (c) 2015 Leonardo Nuñez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
